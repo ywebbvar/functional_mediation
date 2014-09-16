427f25f47dc6ba0209c142336d497682314abed7
Yenny Webb-Vargas <yennywebb@gmail.com>
2014-09-15 17:19:52 -0700

    fixed path to functions

--- SimRevisionR2_YWV.m
+++ SimRevisionR2_YWV.m
@@ -1,6 +1,6 @@
 %% This is my revised version of the file 'SimRevisionR2.m', in which I modify the code to run the simulation on my computer (Lenovo)
 
-addpath 'C:\Users\Yenny\Documents\GitHub\functional_mediation\Original_matlab_code'
+%addpath 'C:\Users\Yenny\Documents\GitHub\functional_mediation\'
 
 tic
 
