6cae4b72af6a77260d54dc4bba47a0827715a7e3
Yenny Webb-Vargas <yennywebb@gmail.com>
2014-11-17 14:28:08 -0500

    finished ssf_Mediation function

--- ssf_Mediation.R
+++ ssf_Mediation.R
@@ -8,7 +8,6 @@
 #' @param outcomeMethod the method for model for mediator. "fRegress" fits a model by penalized from \code{fda} package, and  "fosr2" uses two-step function-on-scalar regression from \code{refund} package
 #' @param nbasis an integer variable specifying the number of basis functions. Argument for \code{\link[fda]{create.bspline.basis}}
 #' @param norder an integer specifying the order of b-splines, which is one higher than their degree. Argument for \code{\link[fda]{create.bspline.basis}}
-#' @param lambda a nonnegative real number specifying the amount of smoothing to be applied to the estimated functional parameters.
 #' @param plot is a logical scalar, if TRUE, the function will produce plots of the a, b and ab paths.
 #' @param boot is a logical scalar, if TRUE, the function will only output the ab path, useful when bootstrap it.
 #' @return gfun A functional regression coefficient corresponding to the gamma path
@@ -19,7 +18,7 @@
 #' @examples
 #' ssf_Mediation(x,y,m,...)
 
-ssf_Mediation <- function(x,y,m,outcomeMethod="fosr2s", nbasis,norder,lambda=1e-8,pen=0.1, plot=FALSE, boot=FALSE){
+ssf_Mediation <- function(x,y,m,outcomeMethod="fosr2s", nbasis,norder,plot=FALSE, boot=FALSE){
   
   require(refund)
   
@@ -42,69 +41,7 @@ ssf_Mediation <- function(x,y,m,outcomeMethod="fosr2s", nbasis,norder,lambda=1e-
   d1_stderr = summary(m_model)$coef["(Intercept)","Std. Error"]
   a_stderr  = summary(m_model)$coef["x","Std. Error"]
   
-  ######################
-  ## Path x, m -> y  ###
-  ######################
-  
-  # Convert time series data to functional data
-  mfd    = Data2fd(y = m,basisobj = basis,argvals = timevec)
-  mfdPar = fdPar(mfd)
-  
-  # Create Design Matrix
-  conbas = create.constant.basis(c(0,T_sup))
-  confd  = fd(matrix(1,nrow=1,ncol=N), conbas)
-  
-  xfdcell      = list()
-  xfdcell[[1]] = confd 
-  xfdcell[[2]] = x
-  
-  # Create basis set for beta functions
-  betacell      = list()
-  betabasis     = create.bspline.basis(c(0,T_sup), nbasis, norder)
-  betafd1       = fd(matrix(0,nrow=nbasis, ncol=1), betabasis)
-  betacell[[1]] = fdPar(betafd1)
-  betafdj       = fd(matrix(0,nrow=nbasis, ncol=1), betabasis)
-  betacell[[2]] = fdPar(betafdj)
-  
-  if(outcomeMethod=="fRegress"){
-    # Solve least-squares equation
-    fRegressCell = fRegress(mfdPar, xfdcell, betacell)
-    betaestcell  = fRegressCell[[4]]
-    afun         = betaestcell[[2]]$fd
-    
-    tfine = seq(0,T_sup, length.out=len)
-    af    = eval.fd(tfine,afun)
-    a     = sum(af)*(tfine[2] - tfine[1])
-    
-    ResM  = (m - eval.fd(tfine, fRegressCell[[5]]$fd))
-    
-    # Calculate Standard Error
-    errmat     = m - eval.fd(tfine, fRegressCell[[5]]$fd)
-    Sigma      = errmat%*%t(errmat)/N  # Originally, there was a 20 here, I assume it is the number of observations N
-    DfdPar     = fdPar(basis, 0, 1)
-    y2cMap     = smooth.basis(timevec,m,DfdPar)$y2cMap
-    stderrCell = fRegress.stderr(fRegressCell, y2cMap, Sigma)
-    tmp        = stderrCell[[1]]
-    
-    d2_stderr = eval.fd(tfine, stderrCell$betastderrlist[[1]]) # Std Error of d2-function
-    g_stderr  = eval.fd(tfine, stderrCell$betastderrlist[[2]]) # Std Error of g-function
-    b_stderr  = eval.fd(tfine, stderrCell$betastderrlist[[3]]) # Std Error of b-function
-    if(plot) plotbeta(betaestlist = fRegressCell$betaestlist, betastderrlist = stderrCell$betastderrlist)
-  }else{
-    if(outcomeMethod=="fosr2s"){
-      tfine = seq(0,T_sup, length.out=len)
-      
-      fit = fosr2s(Y = t(y), cbind(int=1,x=x,m=m), argvals = tfine, nbasis = 15, norder = 4, basistype = "bspline")
-      d2f = fit$est.func[,1]
-      gf  = fit$est.func[,2]
-      bf  = fit$est.func[,3]
-      
-      ResM     = t(t(y) - fit$yhat)
-      d2_stderr = fit$se.func[,1] # Std Error of d2-function
-      g_stderr  = fit$se.func[,2] # Std Error of g-function
-      b_stderr  = fit$se.func[,3] # Std Error of b-function
-    }else{stop("mediatorMethod must be 'fRegress' or 'fosr2s'")}
-  }
+  ResM = m_model$res
   
   ######################
   ## Path x, m -> y  ###
@@ -144,7 +81,7 @@ ssf_Mediation <- function(x,y,m,outcomeMethod="fosr2s", nbasis,norder,lambda=1e-
     gf    = eval.fd(tfine,gfun)
     bf    = eval.fd(tfine,bfun)
     
-    ResM  = (m - eval.fd(tfine, fRegressCell$yhatfdobj$fd))
+    ResY  = (y - eval.fd(tfine, fRegressCell$yhatfdobj$fd))
     
     # Calculate Standard Error
     errmat     = y - eval.fd(tfine, fRegressCell$yhatfdobj$fd)
@@ -154,47 +91,56 @@ ssf_Mediation <- function(x,y,m,outcomeMethod="fosr2s", nbasis,norder,lambda=1e-
     stderrCell = fRegress.stderr(fRegressCell, y2cMap, Sigma)
     tmp        = stderrCell[[1]]
     
-    a_stderr = eval.fd(tfine, tmp[[2]]) # Std Error of a-function
+    d2_stderr = eval.fd(tfine, stderrCell$betastderrlist[[1]]) # Std Error of d2-function
+    g_stderr  = eval.fd(tfine, stderrCell$betastderrlist[[2]]) # Std Error of g-function
+    b_stderr  = eval.fd(tfine, stderrCell$betastderrlist[[3]]) # Std Error of b-function
     plotbeta(betaestlist = fRegressCell$betaestlist, betastderrlist = stderrCell$betastderrlist)
   }else{
-    if(mediatorMethod=="fosr2s"){
+    if(outcomeMethod=="fosr2s"){
       tfine = seq(0,T_sup, length.out=len)
       
-      fit = fosr2s(Y = t(m), cbind(int=1,x=x), argvals = tfine, nbasis = 15, norder = 4, basistype = "bspline")
-      af  = fit$est.func[,2]
-      a   = sum(af)*(tfine[2] - tfine[1])
+      fit = fosr2s(Y = t(y), cbind(int=1,x=x,m=m), argvals = tfine, nbasis = 15, norder = 4, basistype = "bspline")
+      d2f = fit$est.func[,1]
+      gf  = fit$est.func[,2]
+      bf  = fit$est.func[,3]
       
-      ResM     = t(t(m) - fit$yhat)
-      a_stderr = fit$se.func[,2]
-    }else{stop("mediatorMethod must be 'fRegress' or 'fosr2s'")}
+      ResY     = t(t(y) - fit$yhat)
+      d2_stderr = fit$se.func[,1] # Std Error of d2-function
+      g_stderr  = fit$se.func[,2] # Std Error of g-function
+      b_stderr  = fit$se.func[,3] # Std Error of b-function
+    }else{stop("outcomeMethod must be 'fRegress' or 'fosr2s'")}
   }
   
   
-  if(mediatorMethod=="fRegress"){ 
-    abf = a*eval.fd(tfine,bfun)      # ab-functions
-  }else{
-    abf = a*bf
-  }
+   abf = a*bf
   
   # Plot results
   if(plot==TRUE){
     mipar = par()$mfrow
-    par(mfrow=c(3,1))
+    par(mfrow=c(2,2))
   
-    plot(tfine, af, type="l", main="'a' function")
-    lines(tfine, af + 2*a_stderr, col="green")
-    lines(tfine, af - 2*a_stderr, col="green")
+    plot(tfine, d2f, type="l", main="'delta_2' function")
+    lines(tfine, d2f + 2*d2_stderr, col="green")
+    lines(tfine, d2f - 2*d2_stderr, col="green")
   
-    plot(tfine, eval.fd(tfine,bfun), type="l", main="'b' function")
+    plot(tfine, gf, type="l", main="'gamma' function")
+    lines(tfine, gf + 2*g_stderr, col="green")
+    lines(tfine, gf - 2*g_stderr, col="green")
+        
+    plot(tfine, bf, type="l", main="'beta' function")
     lines(tfine, bf + 2*b_stderr, col="green")
     lines(tfine, bf - 2*b_stderr, col="green")
     
-    plot(tfine, abf, type="l", main="'ab' function")
+    plot(tfine, abf, type="l", main="'alpha*beta' function")
     par(mfrow=mipar)
   }
   #plot(x,y)
   
-  if(boot==FALSE) {result = list('afunction' = af, 'a' = a, 'bfunction' = bf, 'abfunction' = abf, 'b' = b, 'ab' = ab, 'cp' = cp, 'Y_intercept' = int, 'x' = x, 'y' = y, 'm' = m,'tfine' = tfine,'b_stderr' = b_stderr,'ResM'= ResM,'ResY'= ResY)
+  m_path = list(delta1 = d1, alpha = a, delta1_stderr = d1_stderr, alpha_stderr= a_stderr, model= m_model)
+  
+  y_path = list(delta2_function = d2f, gamma_function = gf, beta_function=bf, delta2_stderr=d2_stderr, gamma_stderr = g_stderr, beta_stderr=b_stderr)
+  
+  if(boot==FALSE) {result = list(m_path=m_path, y_path = y_path, data = list('x' = x, 'y' = y, 'm' = m),tfine = tfine, ResM= ResM,ResY= ResY)
   }else{
     result = abf
   }
